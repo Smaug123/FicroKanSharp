26366fbc98647e7c963733720990db96e95018af
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-12-29 16:48:12 +0000

    Cleanup

--- FicroKanSharp/Typed.fs
+++ FicroKanSharp/Typed.fs
@@ -3,38 +3,27 @@ namespace FicroKanSharp
 open System
 open System.Reflection
 open FicroKanSharp
-open Microsoft.FSharp.Reflection
+open FSharp.Reflection
 
 type TypedTerm<'a> =
     | Term of Term
     | Literal of 'a
 
-[<CustomEquality ; NoComparison>]
+[<NoComparison>]
 type internal TypeName<'a when 'a : equality> =
     {
         UserType : Type
         FieldValue : 'a
-        OriginalValue : obj
     }
 
     member this.Unbox<'b when 'b : equality> () =
         {
             UserType = this.UserType
             FieldValue = unbox<'b> this.FieldValue
-            OriginalValue = this.OriginalValue
         }
 
-    override this.Equals (other : obj) : bool =
-        match other with
-        | :? TypeName<'a> as other ->
-            this.UserType = other.UserType
-            && this.FieldValue = other.FieldValue
-        | _ -> false
-
-    override this.GetHashCode () : int = hash (this.UserType, this.FieldValue)
-
     override this.ToString () =
-        sprintf "%O<%s>" this.FieldValue this.UserType.Name
+        $"{this.FieldValue}<%s{this.UserType.Name}>"
 
     static member Unify
         (unify : Term -> Term -> State -> State option)
@@ -48,10 +37,10 @@ type internal TypeName<'a when 'a : equality> =
         let unifyMethod =
             t1.UserType.GetMethod (
                 "Unify",
-                System.Reflection.BindingFlags.Static
-                ||| System.Reflection.BindingFlags.Public
-                ||| System.Reflection.BindingFlags.FlattenHierarchy
-                ||| System.Reflection.BindingFlags.NonPublic
+                BindingFlags.Static
+                ||| BindingFlags.Public
+                ||| BindingFlags.FlattenHierarchy
+                ||| BindingFlags.NonPublic
             )
 
         if obj.ReferenceEquals (unifyMethod, null) then
@@ -64,7 +53,7 @@ type internal TypeName<'a when 'a : equality> =
                 $"Incorrect unify return parameter should have been Option<State>: {unifyMethod.ReturnParameter.ParameterType}"
 
         match unifyMethod.GetParameters () with
-        | [| unifyParam ; t1Param ; t2Param ; stateParam |] ->
+        | [| unifyParam ; _t1Param ; _t2Param ; stateParam |] ->
             let wrongParams =
                 [
                     let t =
@@ -154,15 +143,6 @@ type internal TypeName<'a when 'a : equality> =
         let t1 = decompile (t1.Unbox<obj> ()) args1
         let t2 = decompile (t2.Unbox<obj> ()) args2
 
-        (*
-        let t2 =
-            if t2.GetType () = typeof<TypeName<string>> then
-                let t2 = unbox<TypeName<string>> t2
-                let case = unionCases |> Array.find (fun case -> case.Name = t2.FieldValue)
-                FSharpValue.MakeUnion (case, args2 |> List.map (failwith "") |> Array.ofList)
-            else failwith $"oh no 2: {t2.GetType ()}"
-            *)
-
         let result =
             unifyMethod.Invoke (typeof<'a>, [| unify ; t1 ; t2 ; state |])
             |> unbox<State option>
@@ -210,7 +190,6 @@ module TypedTerm =
                 {
                     UserType = resolveGeneric ty
                     FieldValue = fieldU.Name
-                    OriginalValue = t
                 },
                 valuesU
             )
@@ -219,7 +198,6 @@ module TypedTerm =
                 {
                     UserType = resolveGeneric ty
                     FieldValue = t
-                    OriginalValue = t
                 },
                 []
             )
